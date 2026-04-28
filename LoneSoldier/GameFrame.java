a84b17adde2311393f3e7b08fbf1584966ab7020
jeanne mikael tolentino <jeannemikael26@gmail.com>
2026-04-28 23:18:17 +0800

    added minimize and maximize button to make the game big and small when playing the game

--- LoneSoldier/GameFrame.java
+++ LoneSoldier/GameFrame.java
@@ -1,23 +1,33 @@
 import javax.swing.*;
 
 /**
- * GameFrame - Sets up the game window (JFrame)
+ * GameFrame - Main Game Window
  */
 public class GameFrame extends JFrame {
 
-    public static final int WIDTH  = 800;
+    public static final int WIDTH = 800;
     public static final int HEIGHT = 600;
     public static final String TITLE = "LAST OUTPOST: Last Stand";
 
     public GameFrame() {
+
         GamePanel gamePanel = new GamePanel();
 
         this.add(gamePanel);
         this.setTitle(TITLE);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setResizable(false);
-        this.pack();
-        this.setLocationRelativeTo(null); // Center on screen
+
+        // Allow resize / maximize
+        this.setResizable(true);
+
+        this.setSize(WIDTH, HEIGHT);
+
+        // Open at center of screen
+        this.setLocationRelativeTo(null);
+
+        // Show minimize/maximize buttons
+        this.setExtendedState(JFrame.NORMAL);
+
         this.setVisible(true);
 
         gamePanel.requestFocusInWindow();
